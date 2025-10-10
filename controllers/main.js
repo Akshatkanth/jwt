28f67f227749f466aa9b3366059b10f92bc55353
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-10-10 22:38:09 +0530

    main

--- controllers/main.js
+++ controllers/main.js
@@ -1,1 +1,15 @@
 //main
+
+
+const login = async(req, res) => {
+    res.send('Fake login/Register/SignUp Route')
+}
+
+const dashboard = async (req,res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({Msg:`Hello, testing.. `, secret : `Here is your authorised data, your lucky number is: ${luckyNumber}`})
+}
+
+module.exports = {
+    login, dashboard
+}
