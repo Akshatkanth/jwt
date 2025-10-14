5fb23aa6d315aebda9d4ea85c741b7ceec379bcf
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-10-14 13:08:10 +0530

    statusCodes, error handling. Project complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,24 @@
+const jwt = require('jsonwebtoken')
+const {UnautheticatedError} = require('../errors')
 
+const authenticationMiddleware = async (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+    
+        if(!authHeader || !authHeader.startsWith('Bearer')){
+            throw new UnautheticatedError('no token provided')
+        }
+    
+        const token = authHeader.split(' ')[1]
 
-const authenticationMiddleware = 
+        try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const {id, username} = decoded
+        req.user = {id, username}
+        next()
+    } catch (error) {
+        throw new UnautheticatedError('not authorised to access this route')
+    }
+
+} 
+
+module.exports = authenticationMiddleware
--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 const {login, dashboard} = require('../controllers/main')
 
+const authMiddleWare = require('../middleware/auth')
 
-
-router.route('/dashboard').get(dashboard)
+router.route('/dashboard').get(authMiddleWare ,dashboard)
 router.route('/login').post(login)
 
 module.exports = router
